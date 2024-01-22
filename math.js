81c6f34940ecb26698a12ecba3192cfeeed94fad
ArtemKostrikin <96254980+ArtemKostrikin@users.noreply.github.com>
2024-01-22 19:04:16 +0300

    .

---
+++ math.js
@@ -0,0 +1,26 @@
+// Функция сложения двух чисел
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      throw new Error('Деление на ноль невозможно');
+    }
+    return a / b;
+  }
+  
+  module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+  };
